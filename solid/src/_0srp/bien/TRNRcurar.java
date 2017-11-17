6e4c7ce05fa02cd32a1ec10fc8a2c866217a1597
luislama <luislama@espol.edu.ec>
2017-11-16 19:52:58 -0500

    [FIX] Conflicto con el Single-Responsability Principle en la clase Pokemon.java, para esto se añadió [ADD] la clase TRNRcurar.java. [UPD] del archivo instrucciones.docx con la explicación para el conflicto con SRP

---
+++ solid/src/_0srp/bien/TRNRcurar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _0srp.bien;
+
+/**
+ *
+ * @author Luis Lama
+ */
+public class TRNRcurar {
+    
+    public void curar(Pokemon pkmn){
+        pkmn.setPorcentajeVida(100);
+    }
+}
